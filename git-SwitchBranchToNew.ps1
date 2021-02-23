8dbe5872255153d6ebc88f1d42ce2c28a73a207d
Jacek Maciejewski <jacek.maciejewski@zepterit.com>
2021-02-23 21:13:06 +0100

    update 20210223

--- git-SwitchBranchToNew.ps1
+++ git-SwitchBranchToNew.ps1
@@ -1,6 +1,7 @@
 . (Join-path $PSScriptRoot '_Settings.ps1')
 
-$ToBranch = 'JAM-update-BC17-10-20210223'
+$ToBranch = 'JAM-update-BC17-21-20210223'
+#Repr Contract: "Commissions Calculated" cannot be found
 
 $currentLocation = Get-Location
 foreach ($Target in $TargetRepos) {
