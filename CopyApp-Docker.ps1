b5657703d6bb8f4df4617ca49c06121ab6bdcb99
Jacek Maciejewski <jacek.maciejewski@zepterit.com>
2021-03-09 22:58:27 +0100

    update-20210309

---
+++ CopyApp-Docker.ps1
@@ -0,0 +1,20 @@
+docker stop c24 
+
+docker cp "D:\DEV-EXT\APP\Microsoft_Base Application_17.0.16993.1.app" c24:/AppZS/
+
+docker cp "D:\DEV-EXT\APP\Zepter IT_ZS Commission_0.1.0.0.app" c24:/AppZS/
+docker cp "D:\DEV-EXT\APP\Zepter IT_ZS Common_0.1.0.0.app" c24:/AppZS/
+docker cp "D:\DEV-EXT\APP\Zepter IT_ZS GDPR_0.1.0.0.app" c24:/AppZS/
+docker cp "D:\DEV-EXT\APP\Zepter IT_ZS Import Purchase_0.1.0.0.app" c24:/AppZS/
+docker cp "D:\DEV-EXT\APP\Zepter IT_ZS Payment_0.1.0.0.app" c24:/AppZS/
+docker cp "D:\DEV-EXT\APP\Zepter IT_ZS Personal Voucher_0.1.0.0.app" c24:/AppZS/
+docker cp "D:\DEV-EXT\APP\Zepter IT_ZS Representative_0.1.0.0.app" c24:/AppZS/
+docker cp "D:\DEV-EXT\APP\Zepter IT_ZS Sales Contract_0.1.0.0.app" c24:/AppZS/
+docker cp "D:\DEV-EXT\APP\Zepter IT_ZS Sales Item_0.1.0.0.app" c24:/AppZS/
+docker cp "D:\DEV-EXT\APP\Zepter IT_ZS Sample_0.1.0.0.app" c24:/AppZS/
+docker cp "D:\DEV-EXT\APP\Zepter IT_ZS Service_0.1.0.0.app" c24:/AppZS/
+
+docker cp "D:\DEV-EXT\APP\ZITBC170.flf" c24:/AppZS/
+docker cp "D:\DEV-EXT\ps-bc-utils\InstallApp-Docker.ps1" c24:/AppZS/
+
+docker start c24 
