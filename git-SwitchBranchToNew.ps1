8a253a9c910fb1627149476a3654a25813493e6f
Jacek Maciejewski <jacek.maciejewski@zepterit.com>
2021-03-15 19:35:50 +0100

    Update 20210315

--- git-SwitchBranchToNew.ps1
+++ git-SwitchBranchToNew.ps1
@@ -1,10 +1,10 @@
 . (Join-path $PSScriptRoot '_Settings.ps1')
 
-$ToBranch = 'JAM-update-Calendar-SalesProgram-20210309'
+$ToBranch = 'JAM-update-20210315'
 #$ToBranch = 'JAM-update-develop-17.4-20210301'
 #Assembly BOM error
 #Repr Contract: "Commissions Calculated" cannot be found
-
+#Update 20210315
 $currentLocation = Get-Location
 foreach ($Target in $TargetRepos) {
     write-host $Target -ForegroundColor Green
