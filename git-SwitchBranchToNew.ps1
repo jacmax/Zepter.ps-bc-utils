bcf38e78fa7912c24ad75706aca8168e8c099115
Jacek Maciejewski <jacek.maciejewski@zepterit.com>
2021-04-14 14:41:11 +0200

    Update 20210414

--- git-SwitchBranchToNew.ps1
+++ git-SwitchBranchToNew.ps1
@@ -1,6 +1,6 @@
 . (Join-path $PSScriptRoot '_Settings.ps1')
 
-$ToBranch = 'JAM-Update-20210401'
+$ToBranch = 'JAM-Update-20210414'
 #Update gitignore setup 20210319
 #$ToBranch = 'JAM-update-develop-17.4-20210301'
 #Assembly BOM error
