ce547d22295f95fc0d2f3a0a007bfb99dbbb5d28
Jacek Maciejewski <jacek.maciejewski@zepterit.com>
2021-05-24 16:36:30 +0200

    Update 20210524

--- NavExtensions.ps1
+++ NavExtensions.ps1
@@ -7,14 +7,14 @@ function GetNavExtensions {
             [pscustomobject]@{name="SalesItem"       ;version= '0.1.0.1'},
             [pscustomobject]@{name="Representative"  ;version= '0.1.0.0'},
             [pscustomobject]@{name="SalesContract"   ;version= '0.1.0.3'},
-            [pscustomobject]@{name="Payment"         ;version= '0.1.0.2'},
+            [pscustomobject]@{name="Payment"         ;version= '0.1.0.3'},
             [pscustomobject]@{name="PersonalVoucher" ;version= '0.1.0.1'},
             [pscustomobject]@{name="Commission"      ;version= '0.1.0.5'},
             [pscustomobject]@{name="GDPR"            ;version= '0.1.0.1'},
             [pscustomobject]@{name="ImportPurchase"  ;version= '0.1.0.1'},
             [pscustomobject]@{name="Sample"          ;version= '0.1.0.0'},
             [pscustomobject]@{name="Service"         ;version= '0.1.0.3'},
-            [pscustomobject]@{name="ITIntegration"   ;version= '0.1.0.0'}
+            [pscustomobject]@{name="ITIntegration"   ;version= '0.1.0.1'}
         )
     return $NavExtension
 }
