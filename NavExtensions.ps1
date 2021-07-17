0b164e90cfb65e390f1d484007a32687cfc189af
Jacek Maciejewski <jacek.maciejewski@zepterit.com>
2021-07-17 09:32:22 +0200

    Update 20210717

--- NavExtensions.ps1
+++ NavExtensions.ps1
@@ -1,10 +1,10 @@
 function GetNavExtensions {
     $NavExtension =
         @(
-            [pscustomobject]@{name="BaseAppOld"      ;version= '18.0.23013.23795'},
-            [pscustomobject]@{name="BaseApp"         ;version= '18.0.23013.23796'},
+            [pscustomobject]@{name="BaseAppOld"      ;version= '18.0.23013.23797'},
+            [pscustomobject]@{name="BaseApp"         ;version= '18.0.23013.23798'},
             [pscustomobject]@{name="Common"          ;version= '0.1.0.2'},
-            [pscustomobject]@{name="SalesItem"       ;version= '0.1.0.1'},
+            [pscustomobject]@{name="SalesItem"       ;version= '0.1.0.2'},
             [pscustomobject]@{name="Representative"  ;version= '0.1.0.0'},
             [pscustomobject]@{name="SalesContract"   ;version= '0.1.0.3'},
             [pscustomobject]@{name="Payment"         ;version= '0.1.0.3'},
