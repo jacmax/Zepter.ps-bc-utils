0f03c5fa4a4afb5c49f5d93d89c0c43227afc4a5
Jacek Maciejewski <jacek.maciejewski@zepterit.com>
2021-07-26 11:59:06 +0200

    Update 20210726

---
+++ NavExport.ps1
@@ -0,0 +1,261 @@
+﻿Import-Module 'c:\Program Files (x86)\Microsoft Dynamics 365 Business Central\140\RoleTailored Client\Microsoft.Dynamics.Nav.Ide.psm1'
+Import-Module 'c:\Nav\EXPORT.PTM.BC14\NavFindInTextFile.ps1'
+
+$WorkPath = "c:\Nav\EXPORT.PTM.BC14"
+$ServerName = "PLDSQLNAV"
+$DatabaseName = "NAV_PLATF_DEV_140"
+$HUDatabaseName = "NAV_ZHU_DEV_140"
+$ITDatabaseName = "NAV_ITA_DEV_140"
+
+Export-NAVApplicationObject `
+-DatabaseServer $ServerName `
+-DatabaseName $DatabaseName `
+-Path (Join-Path $WorkPath "BC14-PTM.TXT") `
+-ExportToNEwSyntax `
+-Filter 'Id=<2000000000' `
+-Force `
+-verbose
+
+Set-Location -Path $WorkPath
+Remove-Item -Path (Join-Path $WorkPath "export2al") -Recurse
+New-Item -Path $WorkPath -Name "export2al" -ItemType "directory"
+New-Item -Path (Join-Path $WorkPath "export2al") -Name "baseapplication" -ItemType "directory"
+New-Item -Path (Join-Path $WorkPath "export2al") -Name "dotnet" -ItemType "directory"
+New-Item -Path (Join-Path $WorkPath "export2al") -Name "testlibrary" -ItemType "directory"
+New-Item -Path (Join-Path $WorkPath "export2al") -Name "zeptersoft" -ItemType "directory"
+New-Item -Path (Join-Path $WorkPath "export2al") -Name "zs-it" -ItemType "directory"
+New-Item -Path (Join-Path $WorkPath "export2al") -Name "zs-migration" -ItemType "directory"
+New-Item -Path (Join-Path $WorkPath "export2al") -Name "zs-tool" -ItemType "directory"
+
+if (Test-Path "baseapplication") { Remove-Item -Path "baseapplication" -Recurse | Out-null }
+if (Test-Path "testlibrary")     { Remove-Item -Path "testlibrary" -Recurse | Out-null }
+if (Test-Path "zeptersoft")      { Remove-Item -Path "zeptersoft" -Recurse | Out-null }
+if (Test-Path "zs-it")           { Remove-Item -Path "zs-it" -Recurse | Out-null }
+if (Test-Path "zs-migration")    { Remove-Item -Path "zs-migration" -Recurse | Out-null }
+if (Test-Path "zs-tool")         { Remove-Item -Path "zs-tool" -Recurse | Out-null }
+
+$WorkSubPath = (Join-Path (Join-Path $WorkPath "export2al") "baseapplication")
+Export-NAVApplicationObject `
+-DatabaseServer $ServerName `
+-DatabaseName $DatabaseName `
+-ExportToNewSyntax `
+-Path (Join-Path $WorkSubPath "exportedbc14app-part1.txt") `
+-Filter 'Id=1..9999;Version List=*ZS*' `
+-Force `
+-verbose
+
+Export-NAVApplicationObject `
+-DatabaseServer $ServerName `
+-DatabaseName $DatabaseName `
+-ExportToNewSyntax `
+-Path (Join-Path $WorkSubPath "exportedbc14app-part2.txt") `
+-Filter 'Id=10000..49999;Version List=*ZS*' `
+-Force `
+-verbose
+
+Export-NAVApplicationObject `
+-DatabaseServer $ServerName `
+-DatabaseName $DatabaseName `
+-ExportToNewSyntax `
+-Path (Join-Path $WorkSubPath "exportedbc14app-part3.txt") `
+-Filter 'Id=100000..129999;Version List=*ZS*' `
+-Force `
+-verbose
+
+Export-NAVApplicationObject `
+-DatabaseServer $ServerName `
+-DatabaseName $DatabaseName `
+-Path (Join-Path $WorkSubPath "exportedbc14app-part4.txt") `
+-Filter 'Id=140000..20000000;Version List=*ZS*' `
+-Force `
+-verbose
+
+Export-NAVApplicationObject `
+-DatabaseServer $ServerName `
+-DatabaseName $DatabaseName `
+-Path (Join-Path $WorkSubPath "exportedbc14app-part5.txt") `
+-Filter 'Id=20010000..1999999999;Version List=*ZS*' `
+-Force `
+-verbose
+
+Export-NAVApplicationObject `
+-DatabaseServer $ServerName `
+-DatabaseName $DatabaseName `
+-ExportToNewSyntax `
+-Path (Join-Path $WorkSubPath "exportedbc14testobjects.txt") `
+-Filter 'Id=130400..130416;Version List=*ZS*' `
+-Force `
+-verbose
+
+
+#####################################
+# the exported test library objects #
+#####################################
+$WorkSubPath = (Join-Path (Join-Path $WorkPath "export2al") "testlibrary")
+
+Export-NAVApplicationObject `
+-DatabaseServer $ServerName `
+-DatabaseName $DatabaseName `
+-ExportToNewSyntax `
+-Path (Join-Path $WorkSubPath "bc14testlibrary-part1.txt") `
+-Filter 'Id=130000..130399' `
+-Force `
+-verbose
+
+Export-NAVApplicationObject `
+-DatabaseServer $ServerName `
+-DatabaseName $DatabaseName `
+-ExportToNewSyntax `
+-Path (Join-Path $WorkSubPath "bc14testlibrary-part2.txt") `
+-Filter 'Id=130440..139999' `
+-Force `
+-verbose
+
+###########################################
+# the exported ZepterSoft library objects #
+###########################################
+$WorkSubPath = (Join-Path (Join-Path $WorkPath "export2al") "zeptersoft")
+Export-NAVApplicationObject `
+-DatabaseServer $ServerName `
+-DatabaseName $DatabaseName `
+-Path (Join-Path $WorkSubPath "bc14zeptersoft-part1.txt") `
+-Filter 'Id=20000000..20010000' `
+-Force `
+-verbose
+
+Export-NAVApplicationObject `
+-DatabaseServer $ServerName `
+-DatabaseName $DatabaseName `
+-Path (Join-Path $WorkSubPath "bc14zeptersoft-part2.txt") `
+-Filter 'Id=52500..52599' `
+-Force `
+-verbose
+
+Export-NAVApplicationObject `
+-DatabaseServer $ServerName `
+-DatabaseName $DatabaseName `
+-Path (Join-Path $WorkSubPath "bc14zeptersoft-part3.txt") `
+-Filter 'Id=70000' `
+-Force `
+-verbose
+
+$files = Get-ChildItem -Path $WorkSubPath -Filter '*.txt' -Recurse
+foreach ($file in $files) {
+	Find-InTextFile -FilePath $file.FullName
+}    
+
+$WorkSubPath = Join-Path (Join-Path $WorkPath "export2al") "zs-migration"
+Export-NAVApplicationObject `
+-DatabaseServer $ServerName `
+-DatabaseName $HUDatabaseName `
+-Path (Join-Path $WorkSubPath "bc14zeptersoft-migration.txt") `
+-Filter 'Id=60200..60999|64198' `
+-Force `
+-verbose
+
+$WorkSubPath = Join-Path (Join-Path $WorkPath "export2al") "zs-migration"
+Export-NAVApplicationObject `
+-DatabaseServer $ServerName `
+-DatabaseName $DatabaseName `
+-Path (Join-Path $WorkSubPath "bc14zeptersoft-migration-part2.txt") `
+-Filter 'Id=64198' `
+-Force `
+-verbose
+
+$files = Get-ChildItem -Path $WorkSubPath -Filter '*.txt' -Recurse
+foreach ($file in $files) {
+	Find-InTextFile -FilePath $file.FullName
+}    
+
+$WorkSubPath = Join-Path (Join-Path $WorkPath "export2al") "zs-it"
+Export-NAVApplicationObject `
+-DatabaseServer $ServerName `
+-DatabaseName $ITDatabaseName `
+-Path (Join-Path $WorkSubPath "bc14zeptersoft-italy.txt") `
+-Filter 'Id=85000..85999' `
+-Force `
+-verbose
+
+$files = Get-ChildItem -Path $WorkSubPath -Filter '*.txt' -Recurse
+foreach ($file in $files) {
+	Find-InTextFile -FilePath $file.FullName
+}    
+
+$WorkSubPath = Join-Path (Join-Path $WorkPath "export2al") "zs-tool"
+Export-NAVApplicationObject `
+-DatabaseServer $ServerName `
+-DatabaseName $DatabaseName `
+-Path (Join-Path $WorkSubPath "bc14zeptersoft-tool.txt") `
+-Filter 'Type=Codeunit;Id=99000..99999' `
+-Force `
+-verbose
+
+$files = Get-ChildItem -Path $WorkSubPath -Filter '*.txt' -Recurse
+foreach ($file in $files) {
+	Find-InTextFile -FilePath $file.FullName
+}    
+
+
+$WorkSubPathSrc = Join-Path (Join-Path $WorkPath "export2al") "baseapplication"
+$WorkSubPathTrg = Join-Path $WorkPath "baseapplication"
+$WorkSubPathDotnet = Join-Path (Join-Path (Join-Path $WorkPath "export2al") "dotnet") "mydotnet.al"
+Start-Process -FilePath "c:\Program Files (x86)\Microsoft Dynamics 365 Business Central\140\RoleTailored Client\txt2al.exe" `
+-Wait `
+-ArgumentList "--source=$($WorkSubPathSrc) --target=$($WorkSubPathTrg) --injectDotNetAddIns --dotNetAddInsPackage=$($WorkSubPathDotnet) --dotNetTypePrefix=BC --rename --addLegacyTranslationInfo"
+
+$WorkSubPathSrc = Join-Path (Join-Path $WorkPath "export2al") "testlibrary"
+$WorkSubPathTrg = Join-Path $WorkPath "testlibrary"
+Start-Process -FilePath "c:\Program Files (x86)\Microsoft Dynamics 365 Business Central\140\RoleTailored Client\txt2al.exe" `
+-Wait `
+-ArgumentList "--source=$($WorkSubPathSrc) --target=$($WorkSubPathTrg) --injectDotNetAddIns --dotNetTypePrefix=BCTest --rename --addLegacyTranslationInfo"
+
+$WorkSubPathSrc = Join-Path (Join-Path $WorkPath "export2al") "zeptersoft"
+$WorkSubPathTrg = Join-Path $WorkPath "zeptersoft"
+Start-Process -FilePath "c:\Program Files (x86)\Microsoft Dynamics 365 Business Central\140\RoleTailored Client\txt2al.exe" `
+-Wait `
+-ArgumentList "--source=$($WorkSubPathSrc) --target=$($WorkSubPathTrg) --injectDotNetAddIns --dotNetTypePrefix=ZS --rename --addLegacyTranslationInfo"
+
+$WorkSubPathSrc = Join-Path (Join-Path $WorkPath "export2al") "zs-it"
+$WorkSubPathTrg = Join-Path $WorkPath "zs-it"
+Start-Process -FilePath "c:\Program Files (x86)\Microsoft Dynamics 365 Business Central\140\RoleTailored Client\txt2al.exe" `
+-Wait `
+-ArgumentList "--source=$($WorkSubPathSrc) --target=$($WorkSubPathTrg) --injectDotNetAddIns --dotNetTypePrefix=ZS --rename --addLegacyTranslationInfo"
+
+$WorkSubPathSrc = Join-Path (Join-Path $WorkPath "export2al") "zs-migration"
+$WorkSubPathTrg = Join-Path $WorkPath "zs-migration"
+Start-Process -FilePath "c:\Program Files (x86)\Microsoft Dynamics 365 Business Central\140\RoleTailored Client\txt2al.exe" `
+-Wait `
+-ArgumentList "--source=$($WorkSubPathSrc) --target=$($WorkSubPathTrg) --injectDotNetAddIns --dotNetTypePrefix=ZS --rename --addLegacyTranslationInfo"
+
+$WorkSubPathSrc = Join-Path (Join-Path $WorkPath "export2al") "zs-test"
+$WorkSubPathTrg = Join-Path $WorkPath "zs-test"
+Start-Process -FilePath "c:\Program Files (x86)\Microsoft Dynamics 365 Business Central\140\RoleTailored Client\txt2al.exe" `
+-Wait `
+-ArgumentList "--source=$($WorkSubPathSrc) --target=$($WorkSubPathTrg) --injectDotNetAddIns --dotNetTypePrefix=ZS --rename --addLegacyTranslationInfo"
+
+$WorkSubPathSrc = Join-Path (Join-Path $WorkPath "export2al") "zs-tool"
+$WorkSubPathTrg = Join-Path $WorkPath "zs-tool"
+Start-Process -FilePath "c:\Program Files (x86)\Microsoft Dynamics 365 Business Central\140\RoleTailored Client\txt2al.exe" `
+-Wait `
+-ArgumentList "--source=$($WorkSubPathSrc) --target=$($WorkSubPathTrg) --injectDotNetAddIns --dotNetTypePrefix=ZS --rename --addLegacyTranslationInfo"
+
+New-Item -Path (Join-Path $WorkPath "baseapplication") -Name "Table" -ItemType "directory"
+New-Item -Path (Join-Path $WorkPath "baseapplication") -Name "Page" -ItemType "directory"
+New-Item -Path (Join-Path $WorkPath "baseapplication") -Name "Codeunit" -ItemType "directory"
+New-Item -Path (Join-Path $WorkPath "baseapplication") -Name "Report" -ItemType "directory"
+
+$WorkSubPathSrc = Join-Path (Join-Path $WorkPath "baseapplication") "*.Table.al"
+$WorkSubPath = Join-Path (Join-Path $WorkPath "baseapplication") "Table"
+Move-Item -Path $WorkSubPathSrc -Destination $WorkSubPath
+
+$WorkSubPathSrc = Join-Path (Join-Path $WorkPath "baseapplication") "*.Page.al"
+$WorkSubPath = Join-Path (Join-Path $WorkPath "baseapplication") "Page"
+Move-Item -Path $WorkSubPathSrc -Destination $WorkSubPath
+
+$WorkSubPathSrc = Join-Path (Join-Path $WorkPath "baseapplication") "*.Codeunit.al"
+$WorkSubPath = Join-Path (Join-Path $WorkPath "baseapplication") "Codeunit"
+Move-Item -Path $WorkSubPathSrc -Destination $WorkSubPath
+
+$WorkSubPathSrc = Join-Path (Join-Path $WorkPath "baseapplication") "*.Report.al"
+$WorkSubPath = Join-Path (Join-Path $WorkPath "baseapplication") "Report"
+Move-Item -Path $WorkSubPathSrc -Destination $WorkSubPath
---
+++ NavFindInTextFile.ps1
@@ -0,0 +1,84 @@
+Import-Module 'c:\Nav\EXPORT.PTM.BC14\NavTables.ps1'
+Import-Module 'c:\Nav\EXPORT.PTM.BC14\NavCodeunits.ps1'
+
+function Find-InTextFile {
+    <#
+    .SYNOPSIS
+        Performs a find (or replace) on a string in a text file or files.
+    .EXAMPLE
+        PS> Find-InTextFile -FilePath 'C:\MyFile.txt'
+    
+    .PARAMETER FilePath
+        The file path of the text file you'd like to perform a find/replace on.
+    .PARAMETER NewFilePath
+        If a new file with the replaced the string needs to be created instead of replacing
+        the contents of the existing file use this param to create a new file.
+    .PARAMETER Force
+        If the NewFilePath param is used using this param will overwrite any file that
+        exists in NewFilePath.
+    #>
+    [CmdletBinding(DefaultParameterSetName = 'NewFile')]
+    [OutputType()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateScript({Test-Path -Path $_ -PathType 'Leaf'})]
+        [string[]]$FilePath,
+        [Parameter(ParameterSetName = 'NewFile')]
+        [ValidateScript({ Test-Path -Path ($_ | Split-Path -Parent) -PathType 'Container' })]
+        [string]$NewFilePath,
+        [Parameter(ParameterSetName = 'NewFile')]
+        [switch]$Force
+    )
+    begin {
+        $Find = [regex]::Escape($Find)
+    }
+    process {
+        try {
+			$tables = GetNavTables
+			$codeunits = GetNavCodeunits
+            foreach ($File in $FilePath) {
+                if ($NewFilePath) {
+                    if ((Test-Path -Path $NewFilePath -PathType 'Leaf') -and $Force.IsPresent) {
+                        Remove-Item -Path $NewFilePath -Force
+                        $string = Get-Content $File
+                        foreach ($table in $Tables) {
+                            Write-Host $table[0]
+                            $string = $string.replace($table[0],$table[1])
+                        }
+                        $string | Add-Content -Path $NewFilePath -Force
+                    } elseif ((Test-Path -Path $NewFilePath -PathType 'Leaf') -and !$Force.IsPresent) {
+                        Write-Warning "The file at '$NewFilePath' already exists and the -Force param was not used"
+                    } else {
+                        #(Get-Content $File) -replace $Find, $Replace | Add-Content -Path $NewFilePath -Force
+                        $string = Get-Content $File
+                        foreach ($table in $Tables) {
+                            Write-Host $table[0]
+                            $string = $string.replace($table[0],$table[1])
+                        }
+                        $string | Add-Content -Path $NewFilePath -Force
+                    }
+                } else {
+                    #(Get-Content $File) -replace $Find, $Replace | Add-Content -Path "$File.tmp" -Force
+                    $string = (Get-Content $File)
+                    $i = 0
+                    foreach ($table in $Tables) {
+                        $i = $i + 1
+                        Write-Progress -Activity "Updating record names" -Status "Progress:" -PercentComplete ($i/$Tables.count*100)
+                        $string = $string.replace($table[0],$table[1])
+                    }
+                    $i = 0
+                    foreach ($codeunit in $Codeunits) {
+                        $i = $i + 1
+                        Write-Progress -Activity "Updating codeunit names" -Status "Progress:" -PercentComplete ($i/$Codeunits.count*100)
+                        $string = $string.replace($codeunit[0],$codeunit[1])
+                    }
+                    $string | Add-Content -Path "$File.tmp" -Force
+                    Remove-Item -Path $File
+                    Move-Item -Path "$File.tmp" -Destination $File
+                }
+            }
+        } catch {
+            Write-Error $_.Exception.Message
+        }
+    }
+}
