29d05e75e274fb47807486f16fd3be3110996c2a
Jacek Maciejewski <jacek.maciejewski@zepterit.com>
2022-07-26 13:48:49 +0200

    Update 20220726

--- NavExtensions.ps1
+++ NavExtensions.ps1
@@ -6,18 +6,18 @@ function GetNavExtensions {
         [pscustomobject]@{name = "Common"          ; version = '20.0.9.2' },
         [pscustomobject]@{name = "SalesItem"       ; version = '20.0.9.2' },
         [pscustomobject]@{name = "Representative"  ; version = '20.0.9.0' },
-        [pscustomobject]@{name = "SalesContract"   ; version = '20.0.9.5' },
+        [pscustomobject]@{name = "SalesContract"   ; version = '20.0.9.6' },
         [pscustomobject]@{name = "Payment"         ; version = '20.0.9.0' },
         [pscustomobject]@{name = "DataMigration"   ; version = '20.0.9.0' },
         [pscustomobject]@{name = "PersonalVoucher" ; version = '20.0.9.0' },
-        [pscustomobject]@{name = "Commission"      ; version = '20.0.9.0' },
+        [pscustomobject]@{name = "Commission"      ; version = '20.0.9.1' },
         [pscustomobject]@{name = "GDPR"            ; version = '20.0.9.0' },
         [pscustomobject]@{name = "ImportPurchase"  ; version = '20.0.9.0' },
         [pscustomobject]@{name = "Sample"          ; version = '20.0.9.0' },
         [pscustomobject]@{name = "Service"         ; version = '20.0.9.0' },
         [pscustomobject]@{name = "HoldingReport"   ; version = '20.0.9.0' },
         [pscustomobject]@{name = "ITIntegration"   ; version = '20.0.9.0' },
-        [pscustomobject]@{name = "SIIntegration"   ; version = '20.0.9.0' },
+        [pscustomobject]@{name = "SIIntegration"   ; version = '20.0.9.8' },
         [pscustomobject]@{name = "SandboxJAM"      ; version = '20.0.9.0' }
     )
     return $NavExtension
