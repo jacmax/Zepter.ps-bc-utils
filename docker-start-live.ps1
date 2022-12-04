9f929189ced50c03c08cffafbc79386f99cbe079
Jacek Maciejewski <jacek.maciejewski@zepterit.com>
2022-12-04 10:12:32 +0100

    Updated 20221204

--- docker-start-live.ps1
+++ docker-start-live.ps1
@@ -9,16 +9,22 @@ docker stop zsi-live
 docker start zsi-live
 
 docker stop zmk-live
-docker start zmk-live
+#docker start zmk-live
 
 docker stop zba-live
-docker start zba-live
+#docker start zba-live
+
+docker stop zby-live
+#docker start zby-live
 
 docker stop zcz-live
 #docker start zcz-live
 
 docker stop zsk-live
 #docker start zsk-live
+
+docker stop zfr-live
+#docker start zfr-live
 
 docker stop zjo-live
 #docker start zjo-live
