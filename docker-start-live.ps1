7d19082fbf560c6f81ea941dfba19fddf9a3ef77
Jacek Maciejewski <jmaciejewski@zepterit.com>
2024-09-14 11:30:01 +0200

    Updated 20240914

--- docker-start-live.ps1
+++ docker-start-live.ps1
@@ -11,29 +11,27 @@ $env:DOCKER_CONTEXT = 'desktop-windows'
 
 docker stop zsi-live
 docker stop zmk-live
+docker stop zcz-live
+docker stop zfr-live
 
 docker stop zbg-live
 docker stop zch-live
 docker stop zde-live
+docker stop zeg-live
 docker stop zlt-live
 docker stop zus-live
 
 docker stop zhu-live
 docker stop zjo-live
 docker stop zuz-live
+docker stop ita-live
 
 docker stop zba-live
 docker stop zby-live
-docker stop zcz-live
-docker stop zsk-live
-docker stop zfr-live
 
-docker stop cz-bc220
-docker stop fr-bc220
-docker stop ru-bc220
-
-docker stop w1-bc100
-docker stop w1-bc200
-docker stop w1-bc230
-
-#Read-Host -Prompt 'Press any key to continue. . .'
+docker stop w1-bc250
+docker stop cz-bc250
+docker stop fr-bc250
+docker stop de-bc250
+docker stop at-bc250
+# Read-Host -Prompt 'Press any key to continue. . .'
